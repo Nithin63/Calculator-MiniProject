f033a6dbac20a25798a54d2c61de0d11fba8471d
suraj vishwakarma <vishwakarmasuraj0@gmail.com>
2020-11-08 00:52:02 -0500

    Task: pvalue file

---
+++ Statistics/PValue.py
@@ -0,0 +1,14 @@
+from Calculator.Square import squaring
+from Calculator.Division import division
+from Calculator.Addition import addition
+from Calculator.Subtraction import subtraction
+from Statistics.PopulationMean import populationmean
+from Statistics.StandardDeviation import stddev
+from Statistics.ZScore import zscore
+
+
+def pvalue(num):
+    return zscore(num)
+
+# no clue at all how to calculate this
+
